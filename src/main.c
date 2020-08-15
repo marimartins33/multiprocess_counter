cb76732f753e3987ae275d308d5ad8d6929e0e3e
marimartins33 <marianemartins1997@gmail.com>
2020-08-15 00:42:39 -0300

    versao bugada

--- src/main.c
+++ src/main.c
@@ -9,10 +9,14 @@
 
 /*variaveis globais*/
 unsigned long int *entrada;	
-int *contador;	 
-int i =0;
+ 
+int i = 0;
 int k =0;
+int flag;
 
+
+  
+  
 
 
 /*funcao que realiza a leitura dos numeros a serem analisados*/
@@ -28,44 +32,59 @@ void ler_vetor (void){
       
 }
 
-void primo (void){
-  int primo;
-  primo = entrada[i];
+void calculo_primo (int primos){
+ 
+ 
 
 
   int div = 0;
-  
  
-  for (k = 1; k <= primo; k++) {
-    if (primo % k == 0) { 
+ 
+  for (k = 1; k <= primos; k++) {
+    if (primos % k== 0) { 
      div++;
     }
   }
     
-  if (div == 2)
-    *contador = *contador+1;
-  else
+  if (div == 2){
+   flag = 1;
+    }
+  else{
    ;
-
+	}
 }
 
 
  
 int main() {
+
   /*aloca memoria para o vetor entrada*/
   entrada = (unsigned long int*) malloc (sizeof(unsigned long int)*100);
+
+  
+   
  /* Definir flags de protecao e visibilidade de memoria */
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_SHARED | MAP_ANON;
 
 /*cria memoria compartilhada para o vetor entrada*/
- entrada = (unsigned long int*) mmap(NULL, sizeof(unsigned long int)*i,protection, visibility, 0, 0);
+ int *primo;
+  primo = (int*) mmap(NULL, sizeof(int)*i,protection, visibility, 0, 0);
 
 
 /* Cria memoria compartilhada para a variavel contador*/
-
+    int *contador;	
     contador = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
-  /*cria os processos*/
+  
+  /*cria memoaria compartilahda para ponteiro l*/
+  int *l;
+  l = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
+  
+  *l = i;  
+  *primo = entrada[*l];
+  
+  
+ /*cria os processos*/ 
   pid_t filho[N_PROCESSOS];
   
   ler_vetor();
@@ -75,40 +94,53 @@ int main() {
   for (int j=0; j<N_PROCESSOS; j++) {
     filho[j] = fork();
 
+sleep(1); 
     
-    while (i >= 0){
+    while (*l >= 0){
 
 
-      if (filho[j] == 0) {
-      primo();
-      i = i -1;
+      if (j == 0) {
+      calculo_primo(*primo);
+      if (flag==1){
+       *contador = *contador+1;
+       }
+      *l = *l -1;
       
       
       
       }
 
 
-      else if (filho[j] == 1) {
+      else if (j == 1) {
       /*executa no processo filho 1*/
-      primo();
-      i = i -1;
+      calculo_primo(*primo);
+       if (flag==1){
+       *contador = *contador+1;
+       }
+      *l = *l -1;
       
       
       }
 
-      else if (filho[j] == 2) {
+      else if (j== 2) {
       /*executa no processo filho 2*/
-      primo();
-      i = i -1;
+      calculo_primo(*primo);
+       if (flag==1){
+       *contador = *contador+1;
+       }
+      *l = *l -1;
       
       
       }
 
-      else if (filho[j] == 3) {
+      else if (j== 3) {
       /*executa no processo filho 3*/
       
-      primo();
-      i = i -1;
+      calculo_primo(*primo);
+       if (flag==1){
+       *contador = *contador+1;
+       }
+      *l = *l -1;
       
       }
     }  
@@ -123,9 +155,11 @@ int main() {
   for (int j=0; j<N_PROCESSOS; j++) {
     waitpid(filho[j], NULL, 0);
   }
+  
+  
 
 /*informa quantidade de primos*/
-  printf("Quantidade de numeros primos: *c=%d\n", *contador);
+  printf("%d\n", *contador);
   return 0;
 
 }
